287ac4a5ee6b237d2c93b5d958a13a1d6e8fefb2
YunTang1997 <2074402078@qq.com>
2020-07-30 23:14:41 +0800

    修改注释

--- JavaBasics/src/senior/java/io/FileTest.java
+++ JavaBasics/src/senior/java/io/FileTest.java
@@ -8,10 +8,10 @@ import java.util.Date;
 
 /**
  * File类的使用：
- * 1.File类的一个对象，代表一个文件或一个文件目录(俗称：文件夹)。
+ * 1.File类的一个[对象]，代表一个文件或一个文件目录(俗称：文件夹)。
  * 2.File类声明在java.io包下。
  * 3.File类中涉及到关于文件或文件目录的创建、删除、重命名、修改时间、文件大小等方法，
- *   并未涉及到写入或读取文件内容的操作。如果需要读取或写入文件内容，必须使用[IO流]来完成。
+ *   并[未涉及]到写入或读取文件内容的操作。如果需要读取或写入文件内容，必须使用[IO流]来完成。
  * 4.后续File类的对象常会作为参数传递到流的构造器中，指明读取或写入的"终点".
  * @author YunTang
  * @create 2020-07-28 16:29
@@ -170,10 +170,10 @@ public class FileTest {
 
         File file1 = new File("hi.txt");
         if(!file1.exists()){
-            //文件的创建
+            // 文件的创建
             file1.createNewFile();
             System.out.println("创建成功");
-        }else{//文件存在
+        }else{// 文件存在
             file1.delete();
             System.out.println("删除成功");
         }
--- JavaBasics/src/senior/java/io/PathTest.java
+++ JavaBasics/src/senior/java/io/PathTest.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * 1.jdk7.0 时，引入了Path、Paths、Files三个类。
+ * 1.jdk7.0时，引入了Path、Paths、Files三个类。
  * 2.此三个类声明在：java.nio.file包下。
  * 3.Path可以看做是java.io.File类的升级版本。也可以表示文件或文件目录，与平台无关
  *
@@ -22,9 +22,8 @@ public class PathTest {
     //如何使用Paths实例化Path
     @Test
     public void test1() {
-        Path path1 = Paths.get("d:\\nio\\hello.txt");//new File(String filepath)
-
-        Path path2 = Paths.get("d:\\", "nio\\hello.txt");//new File(String parent,String filename);
+        Path path1 = Paths.get("d:\\nio\\hello.txt"); // new File(String filepath)
+        Path path2 = Paths.get("d:\\", "nio\\hello.txt"); // new File(String parent, String filename);
 
         System.out.println(path1);
         System.out.println(path2);
@@ -33,50 +32,56 @@ public class PathTest {
         System.out.println(path3);
     }
 
-    //Path中的常用方法
+    // Path中的常用方法
     @Test
     public void test2() {
         Path path1 = Paths.get("d:\\", "nio\\nio1\\nio2\\hello.txt");
         Path path2 = Paths.get("hello.txt");
 
-//		String toString() ： 返回调用 Path 对象的字符串表示形式
+//		String toString()：返回调用Path对象的字符串表示形式
         System.out.println(path1);
 
-//		boolean startsWith(String path) : 判断是否以 path 路径开始
+//		boolean startsWith(String path)：判断是否以path路径开始
         System.out.println(path1.startsWith("d:\\nio"));
-//		boolean endsWith(String path) : 判断是否以 path 路径结束
+
+//		boolean endsWith(String path)：判断是否以path路径结束
         System.out.println(path1.endsWith("hello.txt"));
-//		boolean isAbsolute() : 判断是否是绝对路径
+
+//		boolean isAbsolute()：判断是否是绝对路径
         System.out.println(path1.isAbsolute() + "~");
         System.out.println(path2.isAbsolute() + "~");
-//		Path getParent() ：返回Path对象包含整个路径，不包含 Path 对象指定的文件路径
+
+//		Path getParent()：返回Path对象包含整个路径，不包含Path对象指定的文件路径
         System.out.println(path1.getParent());
         System.out.println(path2.getParent());
-//		Path getRoot() ：返回调用 Path 对象的根路径
+
+//		Path getRoot()：返回调用Path对象的[根路径]
         System.out.println(path1.getRoot());
         System.out.println(path2.getRoot());
-//		Path getFileName() : 返回与调用 Path 对象关联的文件名
+
+//		Path getFileName()：返回与调用Path对象关联的文件名
         System.out.println(path1.getFileName() + "~");
         System.out.println(path2.getFileName() + "~");
-//		int getNameCount() : 返回Path 根目录后面元素的数量
-//		Path getName(int idx) : 返回指定索引位置 idx 的路径名称
+
+//		int getNameCount()：返回Path根目录后面元素的数量
+//		Path getName(int idx)：返回指定索引位置idx的路径名称
         for (int i = 0; i < path1.getNameCount(); i++) {
             System.out.println(path1.getName(i) + "*****");
         }
 
-//		Path toAbsolutePath() : 作为绝对路径返回调用 Path 对象
+//		Path toAbsolutePath()：作为绝对路径返回调用Path对象
         System.out.println(path1.toAbsolutePath());
         System.out.println(path2.toAbsolutePath());
-//		Path resolve(Path p) :合并两个路径，返回合并后的路径对应的Path对象
+//		Path resolve(Path p)：合并两个路径，返回合并后的路径对应的Path对象
         Path path3 = Paths.get("d:\\", "nio");
         Path path4 = Paths.get("nioo\\hi.txt");
         path3 = path3.resolve(path4);
         System.out.println(path3);
 
-//		File toFile(): 将Path转化为File类的对象
-        File file = path1.toFile();//Path--->File的转换
+//		File toFile()：将Path转化为File类的对象
+        File file = path1.toFile(); // Path--->File的转换
 
-        Path newPath = file.toPath();//File--->Path的转换
+        Path newPath = file.toPath(); // File--->Path的转换
 
     }
 }
