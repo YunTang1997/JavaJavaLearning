f71c22d60a367b92fb3ac0606612c1fa4f9d679e
YunTang1997 <2074402078@qq.com>
2020-07-31 22:55:48 +0800

    URL网络编程代码

---
+++ JavaBasics/src/senior/java/network/URLTest1.java
@@ -0,0 +1,60 @@
+package senior.java.network;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author YunTang
+ * @create 2020-07-31 18:30
+ */
+
+public class URLTest1 {
+    public static void main(String[] args) {
+
+        HttpURLConnection urlConnection = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            URL url = new URL("http://localhost:8080/examples/beauty.jpg");
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            is = urlConnection.getInputStream();
+            fos = new FileOutputStream("JavaBasics/beauty3.jpg");
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = is.read(buffer)) != -1){
+                fos.write(buffer,0,len);
+            }
+
+            System.out.println("下载完成");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            if(is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+        }
+    }
+}
